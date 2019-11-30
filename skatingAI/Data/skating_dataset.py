835ac953980206b4bd6d1a3eea33cb0f6d7159fe
na031 <nadin.katrin.apel@gmail.com>
2019-11-30 07:34:20 +0100

    feature: check empty frames and save as jpg

--- skatingAI/Data/skating_dataset.py
+++ skatingAI/Data/skating_dataset.py
@@ -32,20 +32,45 @@ def get_dataset_flat():
     return np.array(ds_x), np.array(ds_y)
 
 
+def check_empty_frames():
+    ds_names = get_data_names()
+
+    for ds_name in ds_names:
+        _, empty_frames = get_keypoints(ds_name, check_empty_frames=True)
+        frames = get_frames(ds_name)
+        j = 0
+        for idx, frame in enumerate(frames):
+            if len(empty_frames) > 0 and idx == empty_frames[j][1]:
+                cv2.imwrite(
+                    f"{path}/Data/EmptyFrames/{empty_frames[j][0]}_{idx}.jpg", frame)
+                j += 1
+
+        print(
+            f"Successfully parsed {ds_name}\nFound {len(empty_frames)} empty frames.")
+
+
 def get_data_names():
     dirpath = Path(f"{path}/Data/KeyPoints")
 
     return [x.name for x in dirpath.iterdir() if x.is_dir()]
 
 
-def get_keypoints(ds_name):
+def get_keypoints(ds_name, check_empty_frames=False):
     ds_path = Path(f"{path}/Data/KeyPoints/{ds_name}")
     kp_json_paths = [x for x in ds_path.iterdir() if x.is_file()]
     ds_kp = []
+    empty_frames = []
 
-    for file_kp in kp_json_paths:
+    for idx, file_kp in enumerate(kp_json_paths):
         people = get_pose_kp(pd.read_json(file_kp.read_text()))
+
+        if len(people) == 0:
+            empty_frames.append([ds_name, idx])
+
         ds_kp.append(people)
+
+    if check_empty_frames:
+        return np.array(ds_kp), empty_frames
 
     return np.array(ds_kp)
 
@@ -76,12 +101,14 @@ def vid2frames(video_path):
 def get_pose_kp(pose_json):
     people = []
 
+    # todo: check if no people were detected -> then delete frame + kp
+    # keep track how many people in subsequent frames could not be detected and analyze frames
     for person in pose_json.people:
         p = np.array(person['pose_keypoints_2d'])
         # reshape to (x,y,score)
         p = p.reshape((-1, 3))
         # delete not needed score
-        p = p[:, :2]
+        p = p[:, : 2]
         people.append(p)
 
     return np.array(people)
--- skatingAI/run.py
+++ skatingAI/run.py
@@ -1,9 +1,10 @@
-from skatingAI.Data.skating_dataset import get_dataset, get_dataset_flat, get_batch
+from skatingAI.Data.skating_dataset import get_dataset, get_dataset_flat, get_batch, check_empty_frames
 if __name__ == "__main__":
     batch_size = 5
 
-    ds = get_dataset()
-    batches = get_batch(ds, batch_size)
-    ds_x, ds_y = get_dataset_flat()
-    print(ds_x.shape, ds_y.shape)
-    print(f"You've got {len(batches)} random batches")
+    #ds = get_dataset()
+    #batches = get_batch(ds, batch_size)
+    #ds_x, ds_y = get_dataset_flat()
+    check_empty_frames()
+    #print(ds_x.shape, ds_y.shape)
+    #print(f"You've got {len(batches)} random batches")
