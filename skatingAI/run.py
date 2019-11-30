93c12cd5bf68261c3c4b396d7c51931e42dbdf2e
na031 <nadin.katrin.apel@gmail.com>
2019-11-30 16:17:57 +0100

    chore: save empty frames as video

--- skatingAI/run.py
+++ skatingAI/run.py
@@ -1,10 +1,11 @@
-from skatingAI.Data.skating_dataset import get_dataset, get_dataset_flat, get_batch, check_empty_frames
+from skatingAI.Data.skating_dataset import get_dataset, get_dataset_flat, get_batch, check_empty_frames, save_frames_to_video
 if __name__ == "__main__":
     batch_size = 5
 
     #ds = get_dataset()
     #batches = get_batch(ds, batch_size)
     #ds_x, ds_y = get_dataset_flat()
-    check_empty_frames()
+    # check_empty_frames()
+    save_frames_to_video()
     #print(ds_x.shape, ds_y.shape)
     #print(f"You've got {len(batches)} random batches")
