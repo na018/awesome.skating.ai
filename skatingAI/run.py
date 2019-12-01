d69f7daa2c93da12a5bd50a8a75e69e6f752a013
na031 <nadin.katrin.apel@gmail.com>
2019-12-01 08:45:20 +0100

    feature: automatically delete empty frames

--- skatingAI/run.py
+++ skatingAI/run.py
@@ -3,9 +3,10 @@ if __name__ == "__main__":
     batch_size = 5
 
     #ds = get_dataset()
+    ds = get_dataset_flat()
     #batches = get_batch(ds, batch_size)
     #ds_x, ds_y = get_dataset_flat()
-    check_empty_frames()
+    # check_empty_frames()
     # save_frames_to_video()
     #print(ds_x.shape, ds_y.shape)
     #print(f"You've got {len(batches)} random batches")
